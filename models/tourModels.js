50612230f8c0e6dfbcca1a416cf302ee7c00a5a9
Ritesh Ranjan Mishra <riteshranjanmishra938@gmail.com>
2024-01-26 02:02:41 +0530

    addded custom validator

--- models/tourModels.js
+++ models/tourModels.js
@@ -1,87 +1,116 @@
-const mangoose = require('mongoose')
-const slugify = require('slugify')
+const mangoose = require('mongoose');
+const slugify = require('slugify');
+const validator = require('validator')
 
-const tourSchema = new mangoose.Schema({
-    name:{
-      type:String,
-      required:[true,'A tour must have name'],
-      unique:true
+const tourSchema = new mangoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have name'],
+      unique: true,
+      maxlength:[40,'A tour name must have less or equal than 40 characters'],
+      minlength:[10,'A tour name must have greater or equal than 10 characters'],
     },
-    slug:String,
-    duration:{
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have difficulty level'],
+      enum:{
+        values:['easy','medium','difficult'],
+        message:'difficuly can have either easy, medium or difficult'
+      }
+    },
+    ratingAverage: {
+      type: Number,
+      default: 4.5,
+      min:[1.0,'A rating must be more than 1.0'],
+      max:[5.0,'A rating must be less than or equal to 5.0']
+    },
+    ratingQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have price'],
+    },
+    priceDiscount:{
       type:Number,
-      required:[true,'A tour must have a duration']
-    },
-    maxGroupSize:{
+      validate:{
+        validator:function(val){
 
-      type:Number,
-      required:[true,'A tour must have group size']
+          //it will only work when we are creating new tour not on updating existing tour
+          return val<this.price
+        },
+        message:'Discount price should be less than actual price'
+      }
     },
-    difficulty:{
-      type:String,
-      required:[true,'A tour must have difficulty level']
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a summary'],
     },
-    ratingAverage:{
-      type:Number,
-      default:4.5
+    description: {
+      type: String,
+      trim: true,
     },
-    ratingQuantity:{
-      type:Number,
-      default:0
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a image cover'],
     },
-    price:{
-      type:Number,
-      required:[true,'A tour must have price']
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
     },
-    priceDiscount:Number,
-    summary:{
-      type:String,
-      trim:true,
-      required:[true,'A tour must have a summary']
+    startDate: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
     },
-    description:{
-      type:String,
-      trim:true
+  },
+  {
+    toJSON: {
+      virtuals: true,
     },
-    imageCover:{
-      type:String,
-      required:[true,'A tour must have a image cover']
+    toObject: {
+      virtuals: true,
     },
-    images:[String],
-    createdAt:{
-      type:Date,
-      default:Date.now(),
-      select:false
-    },
-    startDate:[Date]
-  },{
-    toJSON:{
-      virtuals:true
-    },
-    toObject:{
-      virtuals:true
-    }
-  })
+  },
+);
 
-  tourSchema.virtual('durationWeeks').get(function(){
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
+});
 
-    return this.duration / 7
-  })
-  
-  // tourSchema.pre('save',function(next){
+// tourSchema.pre('save',function(next){
 
-  //   this.slug = slugify(this.name,{lower:true})
+//   this.slug = slugify(this.name,{lower:true})
 
-  //   next()
-  // })
+//   next()
+// })
 
-  // tourSchema.post('save',function(doc,next){
+// tourSchema.post('save',function(doc,next){
 
-  //   console.log(doc)
+//   console.log(doc)
 
-  //   next()
-  // })
+//   next()
+// })
 
-  const Tour = mangoose.model('Tour',tourSchema)
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+  next();
+});
 
-  module.exports = Tour
+const Tour = mangoose.model('Tour', tourSchema);
+
+module.exports = Tour;
